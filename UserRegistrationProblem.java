5b5368992c32d5570b3f054ccd9128da542ff41a
Srikanthnaid <ksru044gmail.com>
2022-09-16 15:33:35 +0530

    Parameterized test for Emails using JUnit

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
 	/*** UC-1 :- As a User need to enter a valid First Name. ***/
-	public static boolean firstNameValidation(String first_name) {
+	public boolean firstNameValidation(String first_name) {
 		
 		String firstNameRegex = "^[A-Z][A-Z a-z]{3,}$";           //first character is Upper case an remaining all either Upper or Lower 
 		
@@ -17,7 +17,7 @@ public class UserRegistrationProblem {
 	}
 	
 	/*** UC-2 :- As a User need to enter a valid Last Name. ***/
-	public static boolean lastNameValidation(String last_name) {
+	public boolean lastNameValidation(String last_name) {
 		
 		String lastNameRegex = "^[A-Z][A-Z a-z]{3,}$";           //first character is Upper case an remaining all either Upper or Lower 
 		
@@ -29,7 +29,7 @@ public class UserRegistrationProblem {
 	}
 	
 	/*** UC-9:- Should clear all email samples provided separately. ***/
-	public static boolean emailValidation(String email) {
+	public boolean emailValidation(String email) {
 		
 		String emailRegex = "^[\\w+-]+(\\.[\\w+-]+)*@[\\w]+(\\.[\\w]+)?(?=(\\.[A-Za-z_]{2,3}$|\\.[a-zA-Z]{2,3}$)).*$";       //UC-9:- All email validations using regular expressions.
 	
@@ -40,7 +40,7 @@ public class UserRegistrationProblem {
 	}
 	
 	/*** UC-4:- As a User need to follow pre defined Mobile Format. ***/
-	public static boolean mobileNumberValidation(String mobileNum) {
+	public boolean mobileNumberValidation(String mobileNum) {
 		
 		String mobileNumRegex = "^[0-9]{2}(\\s){1}[0-9]{10}$";     //example:- 91 XXXXXXXXXX  mobile number format validation.
 	
@@ -51,7 +51,7 @@ public class UserRegistrationProblem {
 	}
 	
 	/*** UC-8:- As a User need to follow pre-defined Password rules. Rule-4:- Has exactly 1 Special Character. ***/
-	public static boolean passwordValidation(String password) {
+	public boolean passwordValidation(String password) {
 		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)(?=.*[a-z]).{8,}$";      //Rule-4:-  Has exactly 1 Special Character.
 		
 		Pattern pattern = Pattern.compile(passwordRegex);
